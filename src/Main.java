f1c3d70fe8cb97f4f217e7b3ba54e5b8c02f8136
Kirill <121629113+kirKirKir22@users.noreply.github.com>
2023-11-05 14:53:12 +0300

    задача решена

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Student student1 = new Student("Kir", 31);
+        Student student2 = new Student("Kir", 30);
+
+        if (student1.equals(student2)){
+            System.out.println("Студенты равны");
+        }else {
+            System.out.println("Студенты не равны");
+
+        }
+        if (student1.hashCode()==student2.hashCode()){
+            System.out.println("Студенты равны");
+        }else {
+            System.out.println("Студенты не равны");
+        }
+
     }
 }
+
